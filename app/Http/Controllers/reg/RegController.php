43ff9c94312c99cba7d8c3940c50156c4e35f630
wh6636 <wh6636@localhost.localdomain>
2019-05-24 11:39:25 +0800

    123

--- app/Http/Controllers/reg/RegController.php
+++ app/Http/Controllers/reg/RegController.php
@@ -96,8 +96,10 @@ class RegController extends Controller
                 $token=$this->logintoken($datain->user_id);
                 $redis_token_key='logintokens:user_id'.$datain->user_id;
                 $rdd='user_id';
-                Redis::set($rdd,$datain->user_id);
-                Redis::expire($rdd,604800);
+                session([$rdd=>$datain->user_id]);
+
+//                echo session($rdd);
+
                 Redis::set($redis_token_key,$token);
                 Redis::expire($redis_token_key,604800);
 
